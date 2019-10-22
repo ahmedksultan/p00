bde7a6a8c10c02700a695934e122c6babc61b199
Tiffany Cao <tcao00@stuy.edu>
2019-10-22 19:43:32 -0400

    created login and signup functions, connected html form to database

--- app.py
+++ app.py
@@ -2,8 +2,8 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 app = Flask(__name__)
 
-username = "Eggs"
-password = "friedrice"
+username = ""
+password = ""
 app.secret_key = "abcd"
 
 import sqlite3   #enable control of an sqlite database
@@ -13,15 +13,52 @@ DB_FILE= "foldoverdata.db"
 db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
 c = db.cursor()               #facilitate db ops
 
-#command = "CREATE TABLE users (username TEXT, password TEXT, stories_edited BLOB, is_admin INTEGER)"
-#c.execute(command)
+command = "CREATE TABLE IF NOT EXISTS users (username TEXT, password TEXT, stories_edited BLOB, is_admin INTEGER)"
+c.execute(command)
 
+db.commit()
+db.close()
+
+#function for adding a user into the users database
+def adduser(username, password, admin):
+    db = sqlite3.connect(DB_FILE)
+    c = db.cursor()
+    command = "INSERT INTO users (username, password, is_admin) VALUES (\"" + username + "\", \"" + password + "\", " + str(admin) + ")"
+    c.execute(command)
+    db.commit()
+    db.close()
+    return "done"
+
+#function for checking if a user's login credentials are correct
+def checkuser(user, pwd):
+    db = sqlite3.connect(DB_FILE)
+    c = db.cursor()
+    command = "SELECT password FROM users WHERE username = \"" + user + "\""
+    cur = c.execute(command)
+    temp = cur.fetchone()
+    if temp != None:
+        checkpass = temp[0]
+        if checkpass == pwd:
+            return "done"
+        else: return "Password is incorrect."
+    else:
+        return "Username does not exist."
+        # command = "SELECT username FROM users WHERE password = \"" + pwd + "\""
+        # cur = c.execute(command)
+        # temp = cur.fetchone()
+        # if temp != None:
+        #     checkuser = temp[0]
+        #     if checkuser == user:
+        #         return "done"
+        #     else: return "Username is incorrect."
+
+#landing page
 @app.route("/")
 
 def start():
     print(app)
     if 'user' in session: #keeps user logged in
-         return redirect (url_for("homepage"))
+          return redirect (url_for("story"))
     else: #for new user
         flash("Please log in.")
         return render_template('landing.html')
@@ -29,9 +66,15 @@ def start():
 @app.route("/auth")
 
 def authenticate():
-    session['user'] = request.args['username']
-    session['password'] = request.args['password']
-    return redirect (url_for("story"))
+    username = request.args['username']
+    password = request.args['password']
+    check = checkuser(username, password)
+    if(check == "done"):
+        session['user'] = username
+        return redirect (url_for("story"))
+    else:
+        flash("" + check)
+        return render_template('landing.html')
 
 @app.route("/signup")
 
@@ -41,8 +84,18 @@ def sign():
 @app.route("/signupcheck")
 
 def signcheck():
-    command = "INSERT INTO users VALUES (\"" + request.args['username'] + "\", \"" + request.args['password'] + "\", "" , 1)"
-    c.execute(command)
+    username = request.args['username']
+    password = request.args['password']
+    passwordagain = request.args['passwordagain']
+    if passwordagain == password:
+        if(adduser(username, password, 0) == "done"):
+            session['user'] = request.args['username']
+            return redirect (url_for("story"))
+        else: return "No"
+    else:
+        flash("Password does not match.")
+        return render_template('signup.html')
+
 
 @app.route("/mystories")
 
@@ -53,6 +106,13 @@ def story():
 
 def find():
     return "Hello"
+
+@app.route("/logout")
+
+def logout():
+    session.pop('user')
+    flash("You have successfully logged out.")
+    return render_template('landing.html')
 
 # @app.route("/auth")
 # def authenticate(): #checks to match user and pass
@@ -67,5 +127,5 @@ if __name__ == "__main__":
     app.debug = True
     app.run()
 
-db.commit() #save changes
-db.close()  #close database
+# db.commit() #save changes
+# db.close()  #close database
